66416e7a72ed91e735da145a73669ae3ccfe7f37
michaelstsnow <michael.st.snow@gmail.com>
2018-11-19 18:05:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,9 @@ class Song
     song_info=filename.split(" - ")
     song_name=song_info[1]
     song_artist=[0]
-    self.class.create_by_name(song_name)
+    new_song=self.class.create_by_name(song_name)
+    new_song.artist=song_artist
+    new_song
   end
 
   def create_from_filename
