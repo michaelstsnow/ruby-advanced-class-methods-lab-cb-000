4cc970fb8c3c8c280225b63bfd8d0f9d3b4e574d
michaelstsnow <michael.st.snow@gmail.com>
2018-11-19 17:44:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,10 @@ class Song
   end
 
   def self.new_by_name(name)
-    new_song=self.create
+    new_song=self.new
+    new_song.name=(name)
+    new_song.save
+    new_song
   end
 
   def self.create_by_name(name)
